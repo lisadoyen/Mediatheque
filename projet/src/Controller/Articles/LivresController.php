ac480061fe2ed07fa50130e8df19230c619e01cc
Arthur Duhoux <arthudhxpro@gmail.com>
2020-11-18 23:17:03 +0100

    ajout isbn ebay

--- projet/src/Controller/Articles/LivresController.php
+++ projet/src/Controller/Articles/LivresController.php
@@ -51,24 +51,34 @@ class LivresController extends AbstractController
      */
     public function getDataFromIsbn($isbn,SerializerInterface $serializer){
 
+        if(strlen($isbn) < 10) return $this->json('faux',400);
+
+        $articleGoogle = $this->getGoogleIsbn($isbn,$serializer);
+        $articleEbay = $this->getEbayIsbn($isbn,$serializer);
+        return $this->json($this->verifyResponseIsbn($articleGoogle,$articleEbay),200, []);
+    }
+    public function getGoogleIsbn($isbn,$serializer){
         $response = file_get_contents('https://www.googleapis.com/books/v1/volumes?q=isbn:' . $isbn);
         $article = $serializer->decode($response,'json');
-        if($article["totalItems"] > 0){
-            return $this->json($this->verifyGoogleResponseIsbn($article["items"][0]["volumeInfo"]),200, []);
-        }
-        $errors['erreur'] = "livre introuvable";
-        return $this->json($errors,400);
+        return $articleGoogle = $article["items"][0]["volumeInfo"] ?? null;
+    }
+    public function getEbayIsbn($isbn,$serializer){
+        $firstEbay = 'https://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=ArthurDu-proktila-PRD-47ccf9c51-93b76bcb&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=';
+        $lastEbay = '&paginationInput.entriesPerPage=6&GLOBAL-ID=EBAY-FR&siteid=71&Content-Type=application/json';
+        $response = file_get_contents($firstEbay.$isbn.$lastEbay);
+        $articleEbay = $serializer->decode($response,'json');
+        return $articleEbay['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][0];
     }
 
-    public function verifyGoogleResponseIsbn($article){
-        $infos['titre'] = $article['title'] ?? '';
-        $infos['sous_titre'] = $article['subtitle'] ?? '';
-        $infos['auteur'] = $article["authors"][0] ?? '';
-        $infos['editeur'] = $article['publisher'] ?? '';
-        $infos['dateDePublication'] = $article['publishedDate'] ?? '';
-        $infos['description'] = $article['description'] ?? '';
-        $infos['isbn'] = $article['industryIdentifiers'][0]["identifier"] ?? '';
-        $infos['image'] = $article['imageLinks']["thumbnail"] ?? '';
+    public function verifyResponseIsbn($articleGoogle,$articleEbay){
+        $infos['titre'] = $articleGoogle['title'] ?? $articleEbay['title'] ?? '';
+        $infos['sous_titre'] = $articleGoogle['subtitle'] ?? '';
+        $infos['auteur'] = $articleGoogle["authors"][0] ?? '';
+        $infos['editeur'] = $articleGoogle['publisher'] ?? '';
+        $infos['dateDePublication'] = $articleGoogle['publishedDate'] ?? '';
+        $infos['description'] = $articleGoogle['description'] ?? '';
+        $infos['isbn'] = $articleGoogle['industryIdentifiers'][0]["identifier"] ?? '';
+        $infos['image'] = $articleGoogle['imageLinks']['thumbnail'] ?? $articleEbay['galleryURL'] ?? '';
         return $infos;
     }
 }
