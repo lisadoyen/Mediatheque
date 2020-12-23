60fa54123da92226770e2200601c3db40269b174
Théo GANARD <theo.ganard@gmail.com>
2020-12-23 14:49:18 +0100

    Ajout CRUD de plusieurs entites

---
+++ projet/src/Form/CategorieType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categorie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategorieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('libelle')
+            ->add('dureeEmpruntMax')
+            ->add('dureeEmpruntMaxNouveaute')
+            ->add('nbEmpruntMax')
+            ->add('nbEmpruntMaxNouveaute')
+            ->add('dureeNouveaute')
+            ->add('genres')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Categorie::class,
+        ]);
+    }
+}
