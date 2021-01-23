350931e5cb90c31564692a3592a54941e225100c
Thibault Grosjean <62742711+ThibaultGrosjean@users.noreply.github.com>
2021-01-23 11:47:45 +0100

    Ajout et suppression d'un article dans le panier

--- projet/src/Controller/FavorisController.php
+++ projet/src/Controller/FavorisController.php
@@ -4,14 +4,16 @@
 namespace App\Controller;
 
 
+use App\Entity\Favoris;
 use App\Repository\ArticleRepository;
 use App\Repository\FavorisRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
 
-/**
- * @Route("/favoris")
- */
+
 class FavorisController extends AbstractController
 {
     public function isFavourite($idArticle, FavorisRepository $favorisRepository, ArticleRepository $articleRepository){
@@ -22,5 +24,111 @@ class FavorisController extends AbstractController
             'favoris' => $favori,
             'article' => $article
         ]);
+    }
+
+    /**
+     * @Route("/favoris/remove", name="remove_all_favoris", methods={"DELETE"})
+     */
+    public function removeAllFavoris( FavorisRepository $favorisRepository, Request $request){
+        if(!$this->isCsrfTokenValid('favoris_delete', $request->get('token'))) {
+            throw new  InvalidCsrfTokenException('Invalid CSRF token delete favoris');
+        }
+        $user = $this->getUser();
+        $favoris = $favorisRepository->findBy(['utilisateur'=>$user]);
+
+        foreach ($favoris as $fav){
+            $this->getDoctrine()->getManager()->remove($fav);
+            $this->getDoctrine()->getManager()->flush();
+        }
+
+        $this->addFlash('notif',"-1");
+
+        return $this->redirectToRoute('favoris');
+    }
+
+    /**
+     * @Route("/{page}/articles/{id}/remove/favoris", name="remove_article_favoris")
+     */
+    public function removeFavoris(SessionInterface $session, ArticleRepository $articleRepository,FavorisRepository $favorisRepository, $id=1, $page){
+        $user = $this->getUser();
+        $fav = $favorisRepository->findOneBy(['article' => $id, 'utilisateur'=>$user]);
+
+        $this->getDoctrine()->getManager()->remove($fav);
+        $this->getDoctrine()->getManager()->flush();
+
+        $this->addFlash('notif',"-1");
+
+        if($page == "list"){
+            $donnees = $session->get('donnees');
+            if(empty($donnees))
+                return $this->redirectToRoute('articles_show');
+            else {
+                if (!empty($donnees['genres'])) {
+                    return $this->redirectToRoute('genres_id_articles_show', ['id' => 1]);
+                } else {
+                    return $this->redirectToRoute('articles_show');
+                }
+            }
+        }
+        if ($page == "detail") {
+            return $this->redirectToRoute('article_details', ['id' => $id]);
+        }
+        return;
+    }
+
+    /**
+     * @Route("/favoris/remove/{id}", name="remove_favoris")
+     */
+    public function removeFavoris2(ArticleRepository $articleRepository,FavorisRepository $favorisRepository, $id=1){
+        $user = $this->getUser();
+        $fav = $favorisRepository->findOneBy(['article' => $id, 'utilisateur'=>$user]);
+
+        $this->getDoctrine()->getManager()->remove($fav);
+        $this->getDoctrine()->getManager()->flush();
+
+        $this->addFlash('notif',"-1");
+
+        return $this->redirectToRoute('favoris');
+    }
+
+    /**
+     * @Route("/favoris", name="favoris")
+     */
+    public function favoris(FavorisRepository $favorisRepository)
+    {
+        $favoris = $favorisRepository->findBy(['utilisateur' => $this->getUser()]);
+
+        return $this->render('users/profil/favoris.html.twig', ['favoris' => $favoris]);
+    }
+
+    /**
+     * @Route("/{page}/favoris/add/{id}", name="add_article_favoris")
+     */
+    public function addFavoris(SessionInterface $session, ArticleRepository $articleRepository, $id=1, $page){
+        $user = $this->getUser();
+
+        $favoris = new Favoris();
+        $favoris->setArticle($articleRepository->find($id));
+        $favoris->setUtilisateur($user);
+        $this->getDoctrine()->getManager()->persist($favoris);
+        $this->getDoctrine()->getManager()->flush();
+
+        $this->addFlash('notif',"+1");
+
+        if($page == "list"){
+            $donnees = $session->get('donnees');
+            if(empty($donnees))
+                return $this->redirectToRoute('articles_show');
+            else {
+                if (!empty($donnees['genres'])) {
+                    return $this->redirectToRoute('genres_id_articles_show', ['id' => 1]);
+                } else {
+                    return $this->redirectToRoute('articles_show');
+                }
+            }
+        }
+        if ($page == "detail") {
+            return $this->redirectToRoute('article_details', ['id' => $id]);
+        }
     }
 }
---
+++ projet/src/Controller/PanierController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Favoris;
+use App\Entity\Panier;
+use App\Repository\ArticleRepository;
+use App\Repository\FavorisRepository;
+use App\Repository\PanierRepository;
+use App\Repository\TypeEnregistrementRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
+
+class PanierController extends AbstractController
+{
+    /**
+     * @Route("/panier", name="panier")
+     */
+    public function panier(PanierRepository $panierRepository)
+    {
+        $panier = $panierRepository->findBy(['utilisateur' => $this->getUser()]);
+        $totalAchat = 0;
+        foreach ($panier as $article){
+            if ($article->getTypeEnregistrement()->getLibelle() =="achat" and $article->getArticle()->getStatut()->getLibelle() != "vendu"){
+                $totalAchat = $totalAchat + $article->getArticle()->getMontantVente();
+            }
+        }
+
+        return $this->render('users/profil/panier.html.twig', ['panier' => $panier, 'totalAchat' => $totalAchat]);
+    }
+
+    /**
+     * @Route("/panier/add/{id}", name="add_article_panier")
+     */
+    public function addArticlePanier(ArticleRepository $articleRepository,TypeEnregistrementRepository $typeEnregistrementRepository, $id=1){
+        $user = $this->getUser();
+        $article = $articleRepository->findOneBy(['id' => $id]);
+        $statut = $article->getStatut()->getLibelle();
+        $lignePanier = new Panier();
+        $lignePanier->setUtilisateur($user);
+        $lignePanier->setArticle($article);
+
+        if ($statut == "empruntable" or $statut =="emprunte") {
+            $lignePanier->setTypeEnregistrement($typeEnregistrementRepository->findOneBy(['libelle'=>'emprunt']));
+        } else if ($statut == "vendable" or $statut =="vendu") {
+            $lignePanier->setTypeEnregistrement($typeEnregistrementRepository->findOneBy(['libelle'=>'achat']));
+        }
+
+        //TODO verfier que l'article n'est pas le panier
+
+        $this->getDoctrine()->getManager()->persist($lignePanier);
+        $this->getDoctrine()->getManager()->flush();
+
+        $this->addFlash('success',"L'article \"".$lignePanier->getArticle()->getTitre()."\" a bien été ajouté de votre panier");
+
+        return $this->redirectToRoute('panier');
+    }
+
+    /**
+     * @Route("/panier/remove/{id}", name="remove_article_panier")
+     */
+    public function removeArticlePanier(PanierRepository $panierRepository, $id=1){
+        $user = $this->getUser();
+        $lignePanier = $panierRepository->findOneBy(['article' => $id, 'utilisateur'=>$user]);
+
+        $this->getDoctrine()->getManager()->remove($lignePanier);
+        $this->getDoctrine()->getManager()->flush();
+
+        $this->addFlash('success',"L'article \"".$lignePanier->getArticle()->getTitre()."\" a bien été supprimé de votre panier");
+
+        return $this->redirectToRoute('panier');
+    }
+
+    /**
+     * @Route("/panier/move/{id}/favoris", name="remove_article_panier_favoris")
+     */
+    public function moveArticlePanierFavoris(PanierRepository $panierRepository, $id=1){
+        $user = $this->getUser();
+        $lignePanier = $panierRepository->findOneBy(['article' => $id, 'utilisateur'=>$user]);
+
+        $favoris = new Favoris();
+        $favoris->setUtilisateur($user);
+        $favoris->setArticle($lignePanier->getArticle());
+
+        $this->getDoctrine()->getManager()->persist($favoris);
+        $this->getDoctrine()->getManager()->remove($lignePanier);
+        $this->getDoctrine()->getManager()->flush();
+
+        $this->addFlash('success',"L'article \"".$lignePanier->getArticle()->getTitre()."\" a bien été supprimé de votre panier et ajouté à vos favoris !");
+
+        return $this->redirectToRoute('panier');
+    }
+
+    /**
+     * @Route("/panier/remove", name="remove_all_article_panier", methods={"DELETE"})
+     */
+    public function viderPanier(PanierRepository $panierRepository, Request $request)
+    {
+        if(!$this->isCsrfTokenValid('panier_delete', $request->get('token'))) {
+            throw new  InvalidCsrfTokenException('Invalid CSRF token delete panier');
+        }
+        $user = $this->getUser();
+        $panier = $panierRepository->findBy(['utilisateur'=>$user]);
+
+        foreach ($panier as $article){
+            $this->getDoctrine()->getManager()->remove($article);
+            $this->getDoctrine()->getManager()->flush();
+        }
+
+        $this->addFlash('success',"Votre panier a bien été vidé !");
+
+        return $this->redirectToRoute('panier');
+    }
+}
