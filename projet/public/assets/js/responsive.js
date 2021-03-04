6b7bedf41fe7b44c7414c6672dd3119f78e7ee1f
lisa <doyenlisa@gmail.com>
2021-03-04 20:23:34 +0100

    ajout responsive page show article sur iphone XR Max

--- projet/public/assets/js/responsive.js
+++
@@ -1,9 +0,0 @@
-
-
-function test_responsive(){
-    var Lscreen=screen.width;
-
-    if(Lscreen<1200) {
-        document.getElementById('responsive-js').className = "col-md-6 col-lg-5 pt-4 pb-4 float-left"
-    }
-}
