fb6918cae8993d7e730d06d302b1f136c70b0932
Thibault Grosjean <62742711+ThibaultGrosjean@users.noreply.github.com>
2021-03-14 09:21:16 +0100

    Correction Dompdf

--- projet/src/Controller/PanierController.php
+++ projet/src/Controller/PanierController.php
@@ -17,14 +17,14 @@ use App\Repository\TypeActionRepository;
 use App\Repository\TypeEnregistrementRepository;
 use App\Repository\UserRepository;
 use App\Service\MailerService;
-use http\Client\Curl\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
-use Symfony\Flex\Options;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 
 class PanierController extends AbstractController
 {
